7ba3bb3fb9ddf785440c60422fbdae4f38cccd39
Denisa <denisatanase@gmail.com>
2019-04-02 20:52:07 +0300

    pag 159

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50,ErrorMessage = "must be < 50.")]
+        [Column ("FirstName")]
+        [Display(Name ="First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name ="Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = " FUll Name")]
+        public string FullName
+        { get
+            {
+                return LastName + " " + FirstMidName;
+            }
+        }
+
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
